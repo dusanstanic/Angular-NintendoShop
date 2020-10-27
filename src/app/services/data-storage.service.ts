8631ea8a6aa0cad62de7cb5f86a8c2df0dc63bd9
Dusan Stanic <dusan.stanic97@hotmail.com>
2020-10-27 07:19:51 +0100

    Update dataStorageServie

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,11 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Game } from '../models/Game.model';
+import { StoreInterface } from '../store/reducer.model';
 import { GameService } from './game.service';
+import * as gameDataActions from '../store/actions/gameData.actions';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: HttpClient, private gameService: GameService) {}
+  constructor(
+    private http: HttpClient,
+    private gameService: GameService,
+    private store: Store<StoreInterface>
+  ) {}
 
   fetchGames() {
     return this.http
@@ -13,6 +20,7 @@ export class DataStorageService {
       .subscribe((response) => {
         console.log(response);
         this.gameService.setGames(response);
+        this.store.dispatch(new gameDataActions.SetGames(response));
       });
   }
 }
