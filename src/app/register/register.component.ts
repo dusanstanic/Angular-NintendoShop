e1349133b8241d253f6389013c83bf1ddf37e7df
Dusan Stanic <dusan.stanic97@hotmail.com>
2020-10-27 15:37:58 +0100

    Update Register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../models/User.model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('f') registerForm: NgForm;
+  genders = ['male', 'female'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    const user: User = this.registerForm.value;
   }
-
 }
