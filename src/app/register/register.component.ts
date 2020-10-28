9c3aa80151673f84114c2afa03360d87aa94127a
Dusan Stanic <dusan.stanic97@hotmail.com>
2020-10-28 08:48:44 +0100

    Update Register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 import { User } from '../models/User.model';
 
 @Component({
@@ -9,13 +11,32 @@ import { User } from '../models/User.model';
 })
 export class RegisterComponent implements OnInit {
   @ViewChild('f') registerForm: NgForm;
+  @Input() hideModal: Function;
   genders = ['male', 'female'];
+  isLoading: boolean = false;
+  errorMessage: string = '';
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 
   onSubmit() {
+    this.isLoading = true;
+
+    if (!this.registerForm.valid) {
+      return;
+    }
+
     const user: User = this.registerForm.value;
+    this.authService.register(user).subscribe(
+      (response) => {
+        console.log(response);
+        this.isLoading = false;
+      },
+      (errorMessage: string) => {
+        this.errorMessage = errorMessage;
+        this.isLoading = false;
+      }
+    );
   }
 }
